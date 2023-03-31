d0b37d408ce381f407b58d25fd1c34192cabf758
Wcarb0ne <121438696+Wcarb0ne@users.noreply.github.com>
2023-03-31 12:41:11 -0300

    Add files via upload
    adicionando tela de menu header e footer construindo css

---
+++ T13/_CRUD/_menu.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../_CRUD/css/PagSitema.css">
+
+    <title>Document</title>
+
+</head>
+<body>
+
+<div class="BaseMenu">
+
+    <div class="headerMenu">
+        <h3><i class="" >Menu</i></h3>
+    </div>
+    <nav class="nav">
+
+    <ul>
+        <li><i class=""></i>Home</li>
+        <li><i class=""></i>Usuario</li>
+        <li><i class=""></i>Categoria</li>
+        <li><i class=""></i>Produto</li>
+        <li><i class=""></i>Sair</li>
+
+    </ul>
+
+    </nav>
+
+
+
+</div>
+
+</body>
+</html>
